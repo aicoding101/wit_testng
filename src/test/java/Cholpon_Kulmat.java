fcc06183f7a8b02d1f5e0e2e094564d85f6259c4
cholpon <kulmatkyzycholpon@gmail.com>
2019-07-09 16:03:44 -0500

    change code

--- src/test/java/Cholpon_Kulmat.java
+++ src/test/java/Cholpon_Kulmat.java
@@ -1,2 +1,10 @@
+import org.graalvm.compiler.hotspot.replacements.HotSpotReplacementsUtil;
+
+import java.sql.SQLOutput;
+
 public class Cholpon_Kulmat {
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+        System.out.println("Bye java");
+    }
 }

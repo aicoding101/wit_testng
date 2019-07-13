fc8854e26488bf1b68373c625015b4f19f141456
airheem <49971710+airheem@users.noreply.github.com>
2019-07-13 17:08:25 -0500

    Update Aisuluu.java

--- src/test/java/tests/Aisuluu.java
+++ src/test/java/tests/Aisuluu.java
@@ -1,8 +1,4 @@
 package tests;
 
 public class Aisuluu {
-    public static void main(String[] args) {
-        System.out.println("It is working");
-        System.out.println("Second code");
-    }
 }
